b0a06d9a8d17c0e4e9a219f80a92419401227f5a
Nikita <nikita.maximtsev@gmail.com>
2022-06-09 20:51:08 +0300

    Implement register logic

--- src/api/login.js
+++
@@ -1,9 +0,0 @@
-//const URL = 'https://api-nodejs-todolist.herokuapp.com/user/login'
-
-import Axios from 'axios'
-
-Vue.prototype.$http = Axios;
-const token = localStorage.getItem('token')
-if (token) {
-  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
-}
